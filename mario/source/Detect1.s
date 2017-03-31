68fb9f3ccec3f9da6a037b3eb857f0ccd3fec990
Josh Dow <joshua.dow@ucalgary.ca>
2017-03-31 00:38:06 -0600

    Add files via upload
    No screen 3 done tonight, but individual blocks done. Will finish screen 3 tomorrow. SHouldnt take long after the struggle tonight

--- mario/source/Detect1.s
+++ mario/source/Detect1.s
@@ -81,7 +81,7 @@ Block1:
     ldrb r0, [r0]
     cmp r0, #0
     beq B2Head
-    ldr r0, =0x263
+    ldr r0, =0x2BD
     cmp r4, r0
     bge B1Head
 
@@ -209,6 +209,9 @@ Block2:
     bge B2Head
 
 B2Head:
+    ldr r0, =0x2BC
+    cmp r6, r0
+    bge Block3
     ldr r0, =0x1BD
     cmp r5, r0
     bge b2c
@@ -359,7 +362,7 @@ Block3:
     add r7, r5, #70
     add r10, r4, #20
     ldr r0, =0x2BC //Left side of block
-    cmp r10, r0      
+    cmp r6, r0      
     blt Block2
     ldr r0, =0x2E9
     cmp r4, r0
@@ -368,10 +371,69 @@ Block3:
     ldrb r0, [r0]
     cmp r0, #1
     bne Block3down
+    ldr r0, =0x2BD
+    cmp r6, r0
+    bge B3Head
+
+B3Head:
+    ldr r0, =0x1BD
+    cmp r5, r0
+    bge b3c
+B3Feet:
+    ldr r0, =0x190
+    cmp r7, r0
+    ble b3c
+B3SideL:
+    ldr r0, =0x14B
+    cmp r5, r0
+    beq b3c
+    ldr r0, =0x2D5
+    cmp r4, r0
+    bge B3SideR
+
+    ldr r0, =mario
+    ldr r1, =0x290
+    str r1, [r0]
+    ldr r0, =0x2BC
+    ldr r1, =0x190
+    bl drawBlock
+    b b3c
+B3SideR:
+    ldr r0, =state3
+    ldrb r0, [r0]
+    cmp r0, #0
+    beq Block3down
+    ldr r0, =0x2E9
+    cmp r4, r0
+    bgt b3c
+    ldr r0, =0x14B
+    cmp r5, r0
+    ble b3c
+    ldr r0, =mario
+    ldr r1, =0x2EA
+    str r1, [r0]
+    ldr r0, =0x2BC
+    ldr r1, =0x190
+    bl drawBlock
+    b s1done
+
+b3c:
+    ldr r0, =0x2BC 
+    cmp r6, r0      
+    blt Block3down
+    ldr r0, =0x2E9
+    cmp r10, r0
+    bge Block3down
+    ldr r0, =state3
+    ldrb r0, [r0]
+    cmp r0, #0
+    beq Block3down
     ldr r0, =0x1C9
     cmp r5, r0
     bge Block3Under
     b Block3Above
+
+
 Block3down:
     ldr r0, =floor
     ldr r1, =0x1E7
